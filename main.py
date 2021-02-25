ca28e0ddb96e345ddcaf7b94ec2ff10d043b49f8
mwl4263 <masonlapine@acd4122904532376873289.wireless.rit.edu>
2021-02-25 15:20:48 -0500

    security update

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request, session
 import time 
 # Below is MySQL code once database is created
 
@@ -15,12 +15,15 @@ import time
 # End MySQL code
 
 app = Flask(__name__)
-
+app.secret_key = 'cammgroup'
 # Successful login page
 @app.route('/success')
+
 def login_success():
-    print("hit")
-    return render_template("success.html")
+    if session['logged_in'] != 'false':
+        return render_template("success.html")
+    else:
+        return redirect(url_for("login"))
 
 # Login failed page
 @app.route('/invalidcredentials')
@@ -35,6 +38,7 @@ def login():
             error = 'Invalid Credentials. Please try again.'
             return redirect(url_for('failure'))
         else:
+            session['logged_in'] = 'true'
             return redirect(url_for('login_success'))
     return render_template("sign_in.html")
 
@@ -42,5 +46,6 @@ def login():
 # As of right now this just redirects to login
 @app.route('/')
 def start():
+    session['logged_in'] = 'false'
     return redirect(url_for("login"))
 

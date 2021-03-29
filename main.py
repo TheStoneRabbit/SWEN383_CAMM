f97d259549f4fc2da87d3fe006f89f2a1075a7a5
mwl4263 <masonlapine@acd4122904532376873289.wireless.rit.edu>
2021-03-28 23:21:31 -0400

    working on dynamically adding data to dash

--- main.py
+++ main.py
@@ -218,6 +218,14 @@ def aView():
 def admin_user_dash():
     if session["permission_level"] == "(0)":
         if session["logged_in"] != 'false':
+            courseData = mydb.cursor(buffered=True)
+            courseData.execute("select * from course")
+            items = courseData.fetchall()
+            htmlRender = [] 
+            for x in items:
+                for i in x:
+                    print(i)
+                print("change")
             return render_template("admin_dash_user.html")
         else: 
             return redirect(url_for("failure"))

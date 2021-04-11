2920fc1c35f311577ec656ee42711b0bfa026091
aag7593 <aag7593@rit.edu>
2021-04-11 17:59:24 -0400

    Naming Conventions for sql in Python
    naming convention consistency across sql

--- main.py
+++ main.py
@@ -51,7 +51,7 @@ def start():
 def home():
     name = mydb.cursor(buffered=True)
     #print(userID)
-    name.execute("select firstName from user where email = "+ "'" + userID + "'")
+    name.execute("SELECT firstName FROM user WHERE email = "+ "'" + userID + "'")
     firstName = name.fetchall()
     #print(firstName)
     firstName = re.sub("[()]|,|'", "", str(userID)) #Removes extra characters
@@ -67,7 +67,7 @@ def home():
 # +++++++++++++++++++++++++++++++++++++++++++++
 @app.route('/login',  methods=['GET', 'POST'])
 def login():
-    mycursor.execute("select email from user")
+    mycursor.execute("SELECT email FROM user")
     numRows = mycursor.rowcount
 
     bad_chars = ["(", ")", ",", "\'"]
@@ -75,7 +75,7 @@ def login():
         rowsUser = mycursor.fetchall()
         # mycursor.close()
         newCursor = mydb.cursor(buffered=True)
-        newCursor.execute("select hashPassword from user")
+        newCursor.execute("SELECT hashPassword FROM user")
         rowsPass = newCursor.fetchall()
         cred_pass_one = False
         cred_pass_two = False
@@ -119,7 +119,7 @@ def login():
             user_type = mydb.cursor(buffered=True)
             subbed_one = re.sub("[()]|,|'", "", str(subbed_one))
             #print(subbed_one)
-            user_type.execute("select typeU from user where email=" + "'" +subbed_one + "'")
+            user_type.execute("SELECT typeU FROM user WHERE email=" + "'" +subbed_one + "'")
             permission_level = user_type.fetchall()
             #print(permission_level)
             subbed_permission = re.sub("(|)|,|'", "", str(permission_level[0]))
@@ -179,7 +179,7 @@ def admin_panel_index():
     if session["permission_level"] == "(0)":
         if session["logged_in"] != 'false':
             courseData = mydb.cursor(buffered=True)
-            courseData.execute("select * from course")
+            courseData.execute("SELECT * FROM course")
             items = courseData.fetchall()
             htmlRender = [] 
             piece = []
@@ -195,7 +195,7 @@ def admin_panel_index():
                 piece = []
                 count = 0
             userName = mydb.cursor(buffered=True)
-            userName.execute("select firstName, lastName, userID from user where email='"+ email+ "'")
+            userName.execute("SELECT firstName, lastName, userID FROM user WHERE email='"+ email+ "'")
             names = userName.fetchall()
             nameRender = []
             for x in names:
@@ -207,11 +207,11 @@ def admin_panel_index():
             
             if request.method == 'POST':
                 deletefromEnrollment = mydb.cursor(buffered=True)
-                sqlTwo = "delete from enrollment where courseID='" + request.form.get("course") + "'"
+                sqlTwo = "DELETE FROM enrollment WHERE courseID='" + request.form.get("course") + "'"
                 
                 deletefromEnrollment.execute(sqlTwo)
                 deletefrom = mydb.cursor(buffered=True)
-                sql = "delete from course where courseID='" + request.form.get("course") + "'"
+                sql = "DELETE FROM course WHERE courseID='" + request.form.get("course") + "'"
                 deletefrom.execute(sql)
                 mydb.commit()
                 return redirect(url_for("admin_panel_index"))
@@ -238,7 +238,7 @@ def admin_panel_add():
         if session["logged_in"] != 'false':
             if request.method == 'POST':
                 insertinto = mydb.cursor(buffered=True)
-                sql = "INSERT INTO user (firstname, lastname, userID, email, hashpassword, typeU) values (%s, %s, %s, %s, %s, %s)"
+                sql = "INSERT INTO user (firstname, lastname, userID, email, hashpassword, typeU) VALUES (%s, %s, %s, %s, %s, %s)"
                 req_pass = str(request.form['password'])
                 pass_encode = hashlib.sha256(req_pass.encode())
                 try:
@@ -266,14 +266,14 @@ def admin_panel_remove():
         if session["logged_in"] != 'false':
             if request.method == 'POST':
                 deletefromStudentGroups = mydb.cursor(buffered=True)
-                sql = "delete from studentGroups where userID = " + str(int(request.form["username"]))
+                sql = "DELETE FROM studentGroups WHERE userID = " + str(int(request.form["username"]))
                 # values = int(request.form["username"])
                 deletefromStudentGroups.execute(sql)
                 deletefromEnrollment = mydb.cursor(buffered=True)
-                sqlThree = "delete from enrollment where userID = " + str(int(request.form["username"]))
+                sqlThree = "DELETE FROM enrollment WHERE userID = " + str(int(request.form["username"]))
                 deletefromEnrollment.execute(sqlThree)
                 deletefromUser = mydb.cursor(buffered=True)
-                sqlTwo = "delete from user where userID = " + str(int(request.form["username"]))
+                sqlTwo = "DELETE FROM user WHERE userID = " + str(int(request.form["username"]))
                 deletefromUser.execute(sqlTwo)
                 mydb.commit()
                 return render_template("entries_removed.html")
@@ -294,7 +294,7 @@ def admin_user_dash():
     if session["permission_level"] == "(0)":
         if session["logged_in"] != 'false':
             allData = mydb.cursor(buffered=True)
-            allData.execute("select firstName, lastName, userID, email from user WHERE typeU=0")
+            allData.execute("SELECT firstName, lastName, userID, email FROM user WHERE typeU=0")
             items = allData.fetchall()
             htmlRender = []
             numOfItems = len(items)
@@ -302,21 +302,21 @@ def admin_user_dash():
             for x in items:
                 for i in x:
                     htmlRender.append(i)
-            allData.execute("select firstName, lastName, userID, email from user WHERE typeU=1")
+            allData.execute("SELECT firstName, lastName, userID, email FROM user WHERE typeU=1")
             items1 = allData.fetchall()
             numOfItems1 = len(items1)
             lenX1 = 4
             for x1 in items1:
                 for i1 in x1:
                     htmlRender.append(i1)
-            allData.execute("select firstName, lastName, userID, email from user WHERE typeU=2")
+            allData.execute("SELECT firstName, lastName, userID, email FROM user WHERE typeU=2")
             items2 = allData.fetchall()
             numOfItems2 = len(items1)
             lenX2 = 4
             for x2 in items2:
                 for i2 in x2:
                     htmlRender.append(i2)
-            allData.execute("select firstName, lastName from user where email='"+ email+ "'")
+            allData.execute("SELECT firstName, lastName FROM user WHERE email='"+ email+ "'")
             names = allData.fetchall()
             nameRender = []
             for x in names:
@@ -346,7 +346,7 @@ def admin_course():
         if session["logged_in"] != 'false':
             if request.method == 'POST':
                 insertinto = mydb.cursor(buffered=True)
-                sql = "INSERT INTO course (courseID, courseName, capacity, courseLoc, courseTimes) values (%s, %s, %s, %s, %s)"
+                sql = "INSERT INTO course (courseID, courseName, capacity, courseLoc, courseTimes) VALUES (%s, %s, %s, %s, %s)"
                 try:
                     values = (request.form["courseID"], request.form["courseName"],int(request.form["capacity"]), request.form["Location"], request.form["times"])
                     insertinto.execute(sql, values)
@@ -390,7 +390,7 @@ def to_course(course):
     if session["permission_level"] == "(0)":
         if session["logged_in"] != 'false':
             getSpecificCourseData = mydb.cursor(buffered=True)
-            getSpecificCourseData.execute("select courseID, courseName, capacity, courseLoc, courseTimes, firstName, LastName, typeU from course join enrollment using(courseID) join user on enrollment.userID = user.userID where courseID='"+ course+ "' order by typeU asc;")
+            getSpecificCourseData.execute("SELECT courseID, courseName, capacity, courseLoc, courseTimes, firstName, LastName, typeU FROM course JOIN enrollment USING(courseID) JOIN user ON enrollment.userID = user.userID WHERE courseID='"+ course+ "' ORDER BY typeU ASC;")
             items = getSpecificCourseData.fetchall()
             classinfo = []
             outerList = []
@@ -471,7 +471,7 @@ def admin_group_dash():
     if session["permission_level"] == "(0)":
         if session["logged_in"] != 'false':
             groupData = mydb.cursor(buffered=True)
-            sql = "select studentGroups.groupID, group_concat(studentGroups.userID) as 'Users in Group', title, group_description from user_group  join studentGroups on studentGroups.groupID = user_group.groupID join user on studentGroups.userID = user.userID where user.userID = studentGroups.userID group by user_group.groupID"
+            sql = "SELECT studentGroups.groupID, group_concat(studentGroups.userID) AS 'Users in Group', title, group_description FROM user_group JOIN studentGroups ON studentGroups.groupID = user_group.groupID JOIN user ON studentGroups.userID = user.userID WHERE user.userID = studentGroups.userID GROUP BY user_group.groupID"
             groupData.execute(sql)
             items = groupData.fetchall()
             htmlRender = []
@@ -513,7 +513,7 @@ def to_group(group):
     if session["permission_level"] == "(0)":
         if session["logged_in"] != 'false':
             getSpecificGroupData = mydb.cursor(buffered=True)
-            getSpecificGroupData.execute("select groupID, title, group_description, group_concat(studentGroups.userID) as Users from user_group join studentGroups using(groupID) join user on studentGroups.userID = user.userID where groupID='"+ group+ "' group by title order by typeU asc;")
+            getSpecificGroupData.execute("SELECT groupID, title, group_description, group_concat(studentGroups.userID) AS Users FROM user_group JOIN studentGroups USING(groupID) JOIN user ON studentGroups.userID = user.userID WHERE groupID='"+ group+ "' GROUP BY title ORDER BY typeU ASC;")
             items = getSpecificGroupData.fetchall()
             groupInfo = []
             outerList = []
@@ -538,7 +538,7 @@ def to_group(group):
                 if request.method == 'POST':
                     print(outerList)
                     deletefrom = mydb.cursor(buffered=True)
-                    sql = "INSERT INTO studentGroups (userID, groupID, post) values (%s, %s, %s)"
+                    sql = "INSERT INTO studentGroups (userID, groupID, post) VALUES (%s, %s, %s)"
                     try:
                         insertinto = mydb.cursor(buffered=True)
                         values = (userCode, groupID, request.form["makePostInput"])
@@ -562,7 +562,7 @@ def add_group():
     if session["permission_level"] == "(0)":
         if session["logged_in"] != 'false':
             groupData = mydb.cursor(buffered=True)
-            groupData.execute("select * from user_group")
+            groupData.execute("SELECT * FROM user_group")
             items = groupData.fetchall()
             htmlRender = [] 
             piece = []
@@ -578,7 +578,7 @@ def add_group():
                 piece = []
                 count = 0
             userName = mydb.cursor(buffered=True)
-            userName.execute("select firstName, lastName from user where email='" + email + "'")
+            userName.execute("SELECT firstName, lastName FROM user WHERE email='" + email + "'")
             names = userName.fetchall()
             nameRender = []
             for x in names:
@@ -590,7 +590,7 @@ def add_group():
             
             if request.method == 'POST':
                 insertinto = mydb.cursor(buffered=True)
-                sql = "INSERT INTO studentGroups (userID, groupID, userPost) values (%s, %s, %s)"
+                sql = "INSERT INTO studentGroups (userID, groupID, userPost) VALUES (%s, %s, %s)"
                 try:
                     values = (5876, groupID, request.form["makePostInput"])
                     insertinto.execute(sql, values)
@@ -624,7 +624,7 @@ def professor_panel_index():
     if session["permission_level"] == "(1)":
         if session["logged_in"] != 'false':
             userName = mydb.cursor(buffered=True)
-            userName.execute("select firstName, lastName, userID from user where email='"+ email+ "'")
+            userName.execute("SELECT firstName, lastName, userID FROM user WHERE email='"+ email+ "'")
             names = userName.fetchall()
             nameRender = []
             for x in names:
@@ -653,11 +653,11 @@ def professor_panel_index():
             
             if request.method == 'POST':
                 deletefromEnrollment = mydb.cursor(buffered=True)
-                sqlTwo = "delete from enrollment where courseID='" + request.form.get("course") + "'"
+                sqlTwo = "DELETE FROM enrollment WHERE courseID='" + request.form.get("course") + "'"
                 
                 deletefromEnrollment.execute(sqlTwo)
                 deletefrom = mydb.cursor(buffered=True)
-                sql = "delete from course where courseID='" + request.form.get("course") + "'"
+                sql = "DELETE FROM course WHERE courseID='" + request.form.get("course") + "'"
                 deletefrom.execute(sql)
                 mydb.commit()
                 return redirect(url_for("professor_panel_index"))

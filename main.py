e7a3551706d625191b0cea4b141fa01dd60b800b
mwl4263 <masonlapine@acd4122904532376873289.wireless.rit.edu>
2021-04-19 10:45:11 -0400

    more changes from mason

--- main.py
+++ main.py
@@ -643,6 +643,7 @@ def professor_panel_index():
 
             courseData = mydb.cursor(buffered=True)
             courseData.execute("SELECT * FROM course JOIN enrollment USING(courseID) JOIN user USING(userID) WHERE userID=" + str(userCode))
+            
             items = courseData.fetchall()
             htmlRender = [] 
             piece = []
@@ -970,7 +971,7 @@ def professor_adding_add_group_request_to_queue(group):
             try:
                 insertinto = mydb.cursor(prepared=True,)
                 add = "'Add'"
-                values = (int(userID), group, add)
+                values = (int(userCode), group, add)
                 insertinto.execute(sql, values)
                 mydb.commit()
                 return render_template("professor_request_successfully_added_to_queue.html")

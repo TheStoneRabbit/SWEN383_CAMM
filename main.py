a10b87df18ed56b4e1534469623f7728faeb8f8a
Conkeegs <conkeegs@gmail.com>
2021-04-26 18:35:39 -0400

    merge conflict

--- main.py
+++ main.py
@@ -958,7 +958,7 @@ def to_group_professor(group):
         if session["logged_in"] != 'false':
 
             getUserPosts = mydb.cursor(buffered=True)
-            getUserPosts.execute("SELECT firstname, lastname, post FROM studentGroups JOIN user USING(userID) WHERE groupid = " + str(groupID) + " ORDER BY postTime")
+            getUserPosts.execute("SELECT firstname, lastname, post FROM studentGroups JOIN user USING(userID) WHERE groupid = " + str(groupID) + " ORDER BY postTime DESC")
             getUserPostsFetch = getUserPosts.fetchall()
             
             getSpecificGroupData = mydb.cursor(buffered=True)
@@ -995,7 +995,7 @@ def to_group_professor(group):
                         mydb.commit()
 
                         getUserPostsNew = mydb.cursor(buffered=True)
-                        getUserPostsNew.execute("SELECT firstname, lastname, post FROM studentGroups JOIN user USING(userID) WHERE groupid = " + str(groupID) + " ORDER BY postTime")
+                        getUserPostsNew.execute("SELECT firstname, lastname, post FROM studentGroups JOIN user USING(userID) WHERE groupid = " + str(groupID) + " ORDER BY postTime DESC")
                         getUserPostsFetch = getUserPostsNew.fetchall()
 
                         return render_template("professor_group.html", groupUsers=usersForGroup, groupInfo=outerList, posts=getUserPostsFetch)
